3d03aa296b05184c437343220bf5a0a99a11a421
tanishq-tanishq <tanishqd31@gmail.com>
2025-11-19 21:36:35 +0530

    Fixed variable name in prime program

--- program4.py
+++ program4.py
@@ -18,7 +18,7 @@ def is_prime(x):
     if x < 2:
         return False
     for i in range(1,x+1):
-        if n % i == 0:
+        if x % i == 0:
             count+=1
     if count<2:
         print("Prime number")
